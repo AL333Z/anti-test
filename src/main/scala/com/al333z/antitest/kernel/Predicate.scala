1f30201d555c26f807ac3cbdef1f13f5f5bb56a4
Alessandro Zanin <alessandro.zanin@ynap.com>
2017-07-17 17:07:03 +0200

    Refactor

--- src/main/scala/com/al333z/antitest/kernel/Predicate.scala
+++ src/main/scala/com/al333z/antitest/kernel/Predicate.scala
@@ -5,34 +5,10 @@ import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
 import cats.syntax.semigroup._
 import cats.syntax.validated._
-import com.al333z.antitest.kernel.FailedAssertion.{AndFailed, NotFailed, OrFailed, PureFailed}
 
 sealed trait FailedAssertion[E] {
   val error: E
   val errorMessage: String
-}
-
-object FailedAssertion {
-
-  case class AndFailed[E](e: E) extends FailedAssertion[E] {
-    override val error: E = e
-    override val errorMessage: String = "and"
-  }
-
-  case class OrFailed[E](e: E) extends FailedAssertion[E] {
-    override val error: E = e
-    override val errorMessage: String = "or"
-  }
-
-  case class NotFailed[E](e: E) extends FailedAssertion[E] {
-    override val error: E = e
-    override val errorMessage: String = "not"
-  }
-
-  case class PureFailed[E](e: E) extends FailedAssertion[E] {
-    override val error: E = e
-    override val errorMessage: String = ""
-  }
 }
 
 sealed trait Predicate[E, A] {
@@ -49,17 +25,29 @@ sealed trait Predicate[E, A] {
     this match {
       case Pure(_, fun) => fun(a)
       case not: Not[E, A] => not.p.run(a) match {
-        case Valid(r) => Invalid(NotFailed(r._2))
+        case Valid(r) => Invalid(new FailedAssertion[E] {
+            override val error: E = r._2
+            override val errorMessage: String = "not"
+          })
         case Invalid(assertion) => Valid((not.description, assertion.error))
       }
       case and: And[E, A] => and.left.run(a) match {
         case Valid(l) => and.right.run(a) match {
           case Valid(r) => Valid(and.description, r._2 |+| r._2)
-          case Invalid(er) => Invalid(AndFailed(er.error))
+          case Invalid(er) => Invalid(new FailedAssertion[E] {
+            override val error: E = er.error
+            override val errorMessage: String = "and"
+          })
         }
         case Invalid(l) => and.right.run(a) match {
-          case Valid(r) => Invalid(AndFailed(l.error))
-          case Invalid(es) => Invalid(AndFailed(l.error |+| es.error))
+          case Valid(r) => Invalid(new FailedAssertion[E] {
+            override val error: E = l.error
+            override val errorMessage: String = "and"
+          })
+          case Invalid(es) => Invalid(new FailedAssertion[E] {
+            override val error: E = l.error |+| es.error
+            override val errorMessage: String = "and"
+          })
         }
       }
       case or: Or[E, A] => or.left.run(a) match {
@@ -67,7 +55,10 @@ sealed trait Predicate[E, A] {
         case Invalid(el) =>
           or.right.run(a) match {
             case Valid(r) => Valid(or.description, r._2)
-            case Invalid(er) => Invalid(OrFailed(el.error |+| er.error))
+            case Invalid(er) => Invalid(new FailedAssertion[E] {
+              override val error: E = el.error |+| er.error
+              override val errorMessage: String = "or"
+            })
           }
       }
     }
@@ -83,7 +74,11 @@ object Predicate {
   def lift[E, A](description: String,
                  failedAssertions: E,
                  p: A => Boolean) =
-    Pure(description, (a: A) => if (p(a)) (description, failedAssertions).valid else PureFailed(failedAssertions).invalid)
+    Pure(description, (a: A) => if (p(a)) (description, failedAssertions).valid
+    else new FailedAssertion[E] {
+      override val error: E = failedAssertions
+      override val errorMessage: String = ""
+    }.invalid)
 
   final case class And[E, A](left: Predicate[E, A], right: Predicate[E, A]) extends Predicate[E, A] {
     override val description: String = left.description + " and " + right.description
