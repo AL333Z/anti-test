275a12eeacd8414393d21e79655072698d63d7a9
Alessandro Zanin <alessandro.zanin@ynap.com>
2017-07-21 15:09:29 +0200

    Force beforeAll evaluation once for every scenario of the feature

--- src/main/scala/com/al333z/antitest/kernel/FeatureRunner.scala
+++ src/main/scala/com/al333z/antitest/kernel/FeatureRunner.scala
@@ -1,6 +1,6 @@
 package com.al333z.antitest.kernel
 
-import cats.{Comonad, MonadError}
+import cats.{Comonad, Eval, MonadError}
 import org.scalatest.FeatureSpecLike
 
 import scala.collection.immutable
@@ -11,9 +11,10 @@ trait FeatureRunner[F[_]] extends FeatureSpecLike {
 
   def runFeature[FeatureDeps](f: Feature[F, FeatureDeps])
                              (implicit me: MonadError[F, Vector[String]], cm: Comonad[F]): Unit = feature(f.description) {
-    val featureDeps: FeatureDeps = f.beforeAll()
+
+    val featureDeps = Eval.later(f.beforeAll())
     val scenarios = f.scenarios
-    val indexes: immutable.Seq[Int] = scenarios.indices
+    val indexes = scenarios.indices
 
     scenarios.zip(indexes).foreach {
       case (s, i) =>
@@ -24,13 +25,14 @@ trait FeatureRunner[F[_]] extends FeatureSpecLike {
           }
         }
         scenario(description) {
-          val scenarioDeps: s.ScenarioDeps = s.before(featureDeps)
-          val result = Try(cm.extract(s.behaviour(featureDeps, scenarioDeps).run)).toEither
+          val fvalue = featureDeps.value
+          val scenarioDeps: s.ScenarioDeps = s.before(fvalue)
+          val result = Try(cm.extract(s.behaviour(fvalue, scenarioDeps).run)).toEither
 
           try {
             s.after(scenarioDeps)
             if(i == (scenarios.length -1))
-              f.afterAll(featureDeps) // FIXME we can do better than this
+              f.afterAll(fvalue) // FIXME we can do better than this
           }
           finally {
             verify(result)
